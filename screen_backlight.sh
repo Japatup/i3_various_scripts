d16e197af6e44301c8de817ac0878e6f58a0fd77
japatup <japatup@protonmail.com>
2018-11-27 22:22:13 +0100

    mod screen_backlight default values
    (for min_bightness and step)

--- screen_backlight.sh
+++ screen_backlight.sh
@@ -9,32 +9,33 @@
 ## de la même façon que kbd_backlight, ce script sera accessible par lien symbolique depuis /usr/local/bin
 
 # min que l'on souhaite autoriser (le max est déjà géré par xbacklight et vaut 100
-min=5
+min=1
 
 # step: represent the difference between previous and next brightness
-step=10
+step=2
 previous=$(xbacklight -get)
 
 if [[ $1 == "inc" ]]
 then 
-	echo "inc reconnu !  : on laisse xbacklight calculer seul la valeur d'arrivee"
+	#echo "inc reconnu !  : on laisse xbacklight calculer seul la valeur d'arrivee"
 	xbacklight -inc $step
 
 elif [[ $1 == "dec" ]]
 then
-	echo "dec reconnu ! "
+	#echo "dec reconnu ! "
 
 	next=$(echo "$previous - $step" |bc -l)
 
-	echo "next vaut desormais : $next"
+	#echo "next vaut desormais : $next"
 	if [[ $(bc -l <<<"$next <= $min") = 1 ]]
 	then 
-		echo "on ne diminuera pas davantage"
+		:
+		#echo "on ne diminuera pas davantage"
 	else
 		xbacklight -dec $step
 	fi
 else
-	echo "argument ni inc ni dec"
+	#echo "argument ni inc ni dec"
 	exit 1
 fi
 exit 0
